96393bc62199a93527cfc73a7cb480b9698ce8e6
Ian Culver <iculver@login3.ufhpc>
2020-10-19 00:18:01 -0400

    Got something out nonfunctional though

---
+++ Q5.py
@@ -0,0 +1,24 @@
+import time
+for slice in settime
+  time.sleep(5)
+#ill assume the input is sent as thru a data file
+#Tianeptine code :C
+  waterbank={}
+  for interval in liveupdater
+    try 
+     Data = open(/File, "r")
+    except
+     print("No data received")
+   interval.split(',')[1]
+#Trying to get one column from the csv file
+    if interval.split(',')[1] > 5
+    print("Danger high tide in excess of 5.0")
+    else
+     continue
+  waterbank.append(interval)
+#Not really sure where to go from here, I want to compare the previous variable
+#in the waterbank to the next line but I couldnt find anything on that maybe couldve
+#imported a delta program?? 
+  
+
+
